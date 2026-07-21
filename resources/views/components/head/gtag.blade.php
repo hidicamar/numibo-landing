81a722c10ba34bbf5ec749ea9a82ae76f66c9d73
Amar Hidić <amar.hidic@hsplus.si>
2026-07-21 09:17:42 +0200

    Update gtag

--- resources/views/components/head/gtag.blade.php
+++ resources/views/components/head/gtag.blade.php
@@ -1,9 +1,9 @@
 <!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-X4V9MNLRHM"></script>
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-WW5M9D8J2D"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
-  gtag('config', 'G-X4V9MNLRHM');
+  gtag('config', 'G-WW5M9D8J2D');
 </script>
